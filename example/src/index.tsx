509d0cb3001dcdfd4332ef19bd723eaf35cba372
USER <obichilaka@gmail.com>
2024-08-12 15:56:12 +0100

    updates Bug fixing

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,13 +1,58 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "react-select-africa-country-state-lga";
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+import { ReactSelectAfricaCountryStateLga } from 'react-select-africa-country-state-lga'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement,
-);
+const App: React.FC = () => {
+  const [selectedRegion, setSelectedRegion] = useState<string>('')
+  const [selectedCountry, setSelectedCountry] = useState<string>('')
+  const [selectedState, setSelectedState] = useState<string>('')
+  const [selectedLga, setSelectedLga] = useState<string>('')
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+  const regionChangedHandler = (val: string) => {
+    console.log('Selected Region', val)
+    setSelectedRegion(val)
+  }
+
+  const countryChangedHandler = (val: string) => {
+    console.log('Selected Country', val)
+    setSelectedCountry(val)
+  }
+
+  const stateChangedHandler = (val: string) => {
+    console.log('Selected State', val)
+    setSelectedState(val)
+  }
+
+  const lgaChangedHandler = (val: string) => {
+    console.log('Selected LGA', val)
+    setSelectedLga(val)
+  }
+
+  return (
+    <React.StrictMode>
+      <div>
+        <h2>Select African Region, Country, State, and LGA</h2>
+        <ReactSelectAfricaCountryStateLga
+          region={selectedRegion}
+          country={selectedCountry}
+          state={selectedState}
+          lga={selectedLga}
+          setRegion={setSelectedRegion}
+          setCountry={setSelectedCountry}
+          setState={setSelectedState}
+          setLga={setSelectedLga}
+          regionVal={regionChangedHandler}
+          countryVal={countryChangedHandler}
+          stateVal={stateChangedHandler}
+          lgaVal={lgaChangedHandler}
+        />
+      </div>
+    </React.StrictMode>
+  )
+}
+
+// Rendering the application
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+root.render(<App />)
+
+export default App
---
+++ src/index.tsx
@@ -0,0 +1,105 @@
+import React from 'react'
+import styles from './styles.module.css'
+import data from './data'
+
+interface ReactSelectAfricaCountryStateLgaProps {
+  region: string
+  country: string
+  state: string
+  lga: string
+  setRegion: (region: string) => void
+  setCountry: (country: string) => void
+  setState: (state: string) => void
+  setLga: (lga: string) => void
+  regionVal: (region: string) => void
+  countryVal: (country: string) => void
+  stateVal: (state: string) => void
+  lgaVal: (lga: string) => void
+  regionClassName?: string
+  countryClassName?: string
+  stateClassName?: string
+  lgaClassName?: string
+  regionPlaceholder?: string
+  countryPlaceholder?: string
+  statePlaceholder?: string
+  lgaPlaceholder?: string
+}
+
+export const ReactSelectAfricaCountryStateLga: React.FC<ReactSelectAfricaCountryStateLgaProps> = (props) => {
+  const regionList = data.regions.map((region) => ({
+    name: region.name,
+  }))
+
+  const countryList = props.region ? data.regions.find((r) => r.name === props.region)?.countries || [] : []
+
+  const stateList = props.country ? countryList.find((c) => c.name === props.country)?.states || [] : []
+
+  const lgaList = props.state ? stateList.find((s) => s.name === props.state)?.lgas || [] : []
+
+  const handleRegionSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const region = e.target.value
+    props.regionVal(region)
+    props.setRegion(region)
+    props.setCountry('')
+    props.setState('')
+    props.setLga('')
+  }
+
+  const handleCountrySelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const country = e.target.value
+    props.countryVal(country)
+    props.setCountry(country)
+    props.setState('')
+    props.setLga('')
+  }
+
+  const handleStateSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const state = e.target.value
+    props.stateVal(state)
+    props.setState(state)
+    props.setLga('')
+  }
+
+  const handleLgaSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const lga = e.target.value
+    props.lgaVal(lga)
+    props.setLga(lga)
+  }
+
+  return (
+    <div>
+      <select onChange={handleRegionSelect} className={props.regionClassName || styles.test} value={props.region}>
+        <option value=''>{props.regionPlaceholder || 'Select Region'}</option>
+        {regionList.map((region, key) => (
+          <option key={key} value={region.name}>
+            {region.name}
+          </option>
+        ))}
+      </select>
+      <select onChange={handleCountrySelect} className={props.countryClassName || styles.test} value={props.country}>
+        <option value=''>{props.countryPlaceholder || 'Select Country'}</option>
+        {countryList.map((country, key) => (
+          <option key={key} value={country.name}>
+            {country.name}
+          </option>
+        ))}
+      </select>
+      <select onChange={handleStateSelect} className={props.stateClassName || styles.test} value={props.state}>
+        <option value=''>{props.statePlaceholder || 'Select State'}</option>
+        {stateList.map((state, key) => (
+          <option key={key} value={state.name}>
+            {state.name}
+          </option>
+        ))}
+      </select>
+      <select onChange={handleLgaSelect} className={props.lgaClassName || styles.test} value={props.lga}>
+        <option value=''>{props.lgaPlaceholder || 'Select LGA'}</option>
+        {lgaList.map((lga, key) => (
+          <option key={key} value={lga}>
+            {lga}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
